d15f6e618e4727acc18893765c4e1c613690c0eb
Jessica Oliveira <elisa0js@gmail.com>
2023-10-13 10:35:20 -0300

    adicionando animação pokebola

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,20 @@
 const loading = document.querySelector(".loading");
 
 const pokemonName = document.querySelector(".pokemon-name");
-const pokemonNameCard = document.querySelector(".pokemon-name-card");
-
 const pokemonNumber = document.querySelector(".pokemon-number");
-const pokemonNumberCard = document.querySelector(".pokemon-number-card");
-
-const pokemonImage = document.querySelector(".pokemon-image");
 const pokemonGif = document.querySelector(".pokemon-gif");
-
-let pokemonAbilities = document.querySelector(".skills");
-let pokemonType = document.querySelector(".pokemon-type");
-let pokemonAtribute = document.querySelector(".atributes");
 
 const form = document.querySelector(".form");
 const input = document.querySelector(".input-search");
 const btnPrev = document.querySelector(".btn-prev");
 const btnNext = document.querySelector(".btn-next");
 
+const dataCard = document.querySelector(".card");
+const about = document.querySelector(".about")
+
 let searchPokemon = 1;
+
+gerarDadosPokemon("1");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -51,62 +47,36 @@ async function gerarDadosPokemon(pokemon) {
   const data = await gerarPokemon(pokemon);
   if (data) {
     pokemonGif.style.display = "block";
-    pokemonImage.style.display = "block";
-
-    // pokemonNameCard = data.name;
     pokemonName.innerHTML = data.name;
-
     pokemonNumber.innerHTML = data.id;
-    pokemonNumberCard.innerHTML = `#${data.id}`;
-
     pokemonGif.src =
       data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
-        "front_default"
-      ];
-    pokemonImage.src = data.sprites.other.dream_world.front_default;
-
-    const types = data.types.map((typeSlot) => typeSlot.type.name);
-    const [type] = types;
+        "front_default"];
+      
+  const types = data.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
     data.types = types;
     data.type = type;
-    const dataCard = document.querySelector(".card");
-    const about = document.querySelector(".about")
-    
-
    
-   
-                             dataCard.innerHTML=`  
-                                                 
-                                                  <div class="card ${data.type} ">
-                                                 
-                                                  <ul class="types">
-                                                  <li>
-                                                     
-                                                      ${data.types.map((type) => `<span class="type ${type}">${type}</span>`).join('')}
-                                                  </li>
-                                              </ul>
-                                                   
-                                                    <h2 class="pokemon-name-card">${data.name}</h2>
-                                                    <p class="pokemon-number-card">#${data.id}</p>
-                                                    <img class="pokemon-image"src="${data.sprites.other.dream_world.front_default}" alt="${data.name}">
-                                                  </div>`
-                             
-    const skills = data.abilities.map((skill) => skill.ability.name);
-    
-    const [skill1, skill2 = "only one skill"] = skills;
+    dataCard.innerHTML=` <div class="card ${data.type}"> 
+                            <ul class="types">                     
+                              <li>
+                               ${data.types.map((type) => `<span class="type ${type}">${type}</span>`).join('')}                        
+                              </li>
+                            </ul>
+                            <h2 class="pokemon-name-card">${data.name}</h2>
+                            <p class="pokemon-number-card">#${data.id}</p>
+                            <img class="pokemon-image"src="${data.sprites.other.dream_world.front_default}" alt="${data.name}">
+                        </div>`           
+                    
+  const skills = data.abilities.map((skill) => skill.ability.name);
+  const [skill1, skill2 = "only one skill"] = skills;
     data.skill1 = skill1;
     data.skill2 = skill2;
 
-    const statsValues = data.stats.map((stats) => stats.base_stat);
-    const [
-      statValueHp,
-      statValueAtk,
-      statValueDef,
-      statValueSatk,
-      statValueSdef,
-      statValueSpd,
-    ] = statsValues;
-
+  const statsValues = data.stats.map((stats) => stats.base_stat);
+  const [statValueHp, statValueAtk, statValueDef,statValueSatk,statValueSdef,statValueSpd,] = statsValues;
+  
     data.statValueHp = statValueHp;
     data.statValueAtk = statValueAtk;
     data.statValueDef = statValueDef;
@@ -127,11 +97,7 @@ async function gerarDadosPokemon(pokemon) {
                        
                         <ul class="skills"
                             <li class="skill">${data.skill1} - ${data.skill2}</li>
-                            
-                        </ul>
-                    `
-                          
-                            // return data;
+                        </ul>`     
 
     input.value = "";
     searchPokemon = data.id;
@@ -139,6 +105,10 @@ async function gerarDadosPokemon(pokemon) {
     pokemonName.innerHTML = "Not found";
     pokemonNumber.innerHTML = "";
     pokemonGif.style.display = "none";
+    dataCard.style.display = "none";
+    about.style.display = "none";
+    
   }
 }
-gerarDadosPokemon("1");
+
+
